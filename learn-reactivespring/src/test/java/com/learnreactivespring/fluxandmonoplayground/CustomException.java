fb3ee50a0ecd747f588c0a367df9f0750eb80f72
sirAlexander <alexander.shikanga.tindi@gmail.com>
2020-01-29 14:54:11 +0000

    handling errors in a reactive stream

---
+++ learn-reactivespring/src/test/java/com/learnreactivespring/fluxandmonoplayground/CustomException.java
@@ -0,0 +1,19 @@
+package com.learnreactivespring.fluxandmonoplayground;
+
+public class CustomException extends Throwable {
+
+    private String message;
+
+    public CustomException(Throwable e) {
+        this.message = e.getMessage();
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
