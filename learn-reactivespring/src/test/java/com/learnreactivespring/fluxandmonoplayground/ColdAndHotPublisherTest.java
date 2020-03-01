0ae1414bc364957d339e7e79706fd0e9fde87df8
sirAlexander <alexander.shikanga.tindi@gmail.com>
2020-03-01 14:59:16 +0000

    hot vs cold reactive streams

---
+++ learn-reactivespring/src/test/java/com/learnreactivespring/fluxandmonoplayground/ColdAndHotPublisherTest.java
@@ -0,0 +1,44 @@
+package com.learnreactivespring.fluxandmonoplayground;
+
+import org.junit.Test;
+import reactor.core.publisher.ConnectableFlux;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+public class ColdAndHotPublisherTest {
+
+    @Test
+    public void coldPublisherTest() throws InterruptedException {
+
+        Flux<String> stringFlux = Flux.just("A", "B", "C", "D", "E", "F")
+                .delayElements(Duration.ofSeconds(1));
+
+        stringFlux.subscribe(s -> System.out.println("Subscriber 1 : " + s)); // emits value from the beginning
+
+        Thread.sleep(3000);
+
+        stringFlux.subscribe(s -> System.out.println("Subscriber 2 : " + s)); // also emits values from the beginning
+
+        Thread.sleep(4000);
+
+    }
+
+    @Test
+    public void hotPublisherTest() throws InterruptedException {
+
+        Flux<String> stringFlux = Flux.just("A", "B", "C", "D", "E", "F")
+                .delayElements(Duration.ofSeconds(1));
+
+        ConnectableFlux<String> connectableFlux = stringFlux.publish();
+        connectableFlux.connect();
+        connectableFlux.subscribe(s -> System.out.println("Subscriber 1 : " + s));
+
+        Thread.sleep(3000);
+
+        connectableFlux.subscribe(s -> System.out.println("Subscriber 2 : " + s));
+
+        Thread.sleep(4000);
+
+    }
+}
