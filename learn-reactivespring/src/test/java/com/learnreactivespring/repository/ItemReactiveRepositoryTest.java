c3628ad5c25beb0b09799f06edf294e2fc7fa151
sirAlexander <alexander.shikanga.tindi@gmail.com>
2020-03-31 13:34:32 +0100

    added reactive repository '.findAllItems()' JUnit test

---
+++ learn-reactivespring/src/test/java/com/learnreactivespring/repository/ItemReactiveRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.learnreactivespring.repository;
+
+import com.learnreactivespring.document.Item;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+@DataMongoTest
+@RunWith(SpringRunner.class)
+public class ItemReactiveRepositoryTest {
+
+    @Autowired
+    ItemReactiveRepository itemReactiveRepository;
+
+    List<Item> items = Arrays.asList(
+            new Item(null, "Samsung TV", 400.00),
+            new Item(null, "LG TV", 420.00),
+            new Item(null, "Apple Watch", 299.99),
+            new Item(null, "Beats Headphones", 149.99)
+    );
+
+    @Before
+    public void setUp() {
+
+        itemReactiveRepository.deleteAll()
+                .thenMany(Flux.fromIterable(items))
+                .flatMap(itemReactiveRepository::save)
+                .doOnNext(item -> System.out.println(" Inserted Item is : " + item))
+                .blockLast();
+    }
+
+    @Test
+    public void getAllItems() {
+
+        StepVerifier.create(itemReactiveRepository.findAll())
+                .expectSubscription()
+                .expectNextCount(4)
+                .verifyComplete();
+    }
+
+}
