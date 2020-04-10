35787eba8f2f4fb3666c4080874a8ff05b95287e
sirAlexander <alexander.shikanga.tindi@gmail.com>
2020-04-10 20:56:17 +0100

    create an Item Endpoint and associated JUnit test

--- learn-reactivespring/src/main/java/com/learnreactivespring/controller/v1/ItemController.java
+++ learn-reactivespring/src/main/java/com/learnreactivespring/controller/v1/ItemController.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -27,9 +30,15 @@ public class ItemController {
     }
 
     @GetMapping(ITEM_END_POINT_V1 + "/{id}")
-    public Mono<ResponseEntity<Item>> getOneItem(@PathVariable String id){
+    public Mono<ResponseEntity<Item>> getOneItem(@PathVariable String id) {
         return itemReactiveRepository.findById(id)
                 .map(item -> new ResponseEntity<>(item, HttpStatus.OK))
                 .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping(ITEM_END_POINT_V1)
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Item> createItem(@RequestBody Item item) {
+        return itemReactiveRepository.save(item);
     }
 }
--- learn-reactivespring/src/test/java/com/learnreactivespring/controller/v1/ItemControllerTest.java
+++ learn-reactivespring/src/test/java/com/learnreactivespring/controller/v1/ItemControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.Arrays;
@@ -109,7 +110,20 @@ public class ItemControllerTest {
                 .expectStatus().isNotFound();
     }
 
+    @Test
+    public void createItem() {
+        Item item = new Item(null, "Iphone X", 999.99);
 
+        webTestClient.post().uri(ITEM_END_POINT_V1)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .body(Mono.just(item), Item.class)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody()
+                .jsonPath("$.id").isNotEmpty()
+                .jsonPath("$.description").isEqualTo("Iphone X")
+                .jsonPath("$.price").isEqualTo(999.99);
+    }
 
 
 }
