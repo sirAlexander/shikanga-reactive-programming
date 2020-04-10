2ae8d6aea33f75cabf71fa021596b84c6817d778
sirAlexander <alexander.shikanga.tindi@gmail.com>
2020-04-10 17:10:22 +0100

    Initialize the data using CommandLineRunner

---
+++ learn-reactivespring/src/main/java/com/learnreactivespring/initialize/ItemDataInitializer.java
@@ -0,0 +1,40 @@
+package com.learnreactivespring.initialize;
+
+import com.learnreactivespring.document.Item;
+import com.learnreactivespring.repository.ItemReactiveRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class ItemDataInitializer implements CommandLineRunner {
+
+    @Autowired
+    ItemReactiveRepository itemReactiveRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        initialDataSetUp();
+    }
+
+    public List<Item> data() {
+        return Arrays.asList(
+                new Item(null, "Samsung TV", 399.99),
+                new Item(null, "LG TV", 329.99),
+                new Item(null, "Apple Watch", 349.99),
+                new Item("ABC", "Beats Headphones", 19.99)
+        );
+    }
+
+    private void initialDataSetUp() {
+        itemReactiveRepository.deleteAll()
+                .thenMany(Flux.fromIterable(data()))
+                .flatMap(itemReactiveRepository::save)
+                .thenMany(itemReactiveRepository.findAll())
+                .subscribe(item -> System.out.println(" Item inserted from CommandLineRunner : " + item));
+    }
+}
