8d4c4ef90095f21f2f91bac19aee3971039c6108
sirAlexander <alexander.shikanga.tindi@gmail.com>
2020-07-29 19:27:02 +0100

    passing a path variable while using webclient

--- item-client/src/main/java/com/learnreactivespring/controller/ItemClientController.java
+++ item-client/src/main/java/com/learnreactivespring/controller/ItemClientController.java
@@ -2,9 +2,11 @@ package com.learnreactivespring.controller;
 
 import com.learnreactivespring.domain.Item;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class ItemClientController {
@@ -12,7 +14,7 @@ public class ItemClientController {
     WebClient webClient = WebClient.create("http://localhost:8080");
 
     @GetMapping("/client/retrieve")
-    public Flux<Item> getAllItemsUsingRetrieve(){
+    public Flux<Item> getAllItemsUsingRetrieve() {
 
         return webClient.get().uri("/v1/items")
                 .retrieve()
@@ -21,12 +23,31 @@ public class ItemClientController {
     }
 
     @GetMapping("/client/exchange")
-    public Flux<Item> getAllItemsUsingExchange(){
+    public Flux<Item> getAllItemsUsingExchange() {
 
         return webClient.get().uri("/v1/items")
                 .exchange()
                 .flatMapMany(clientResponse -> clientResponse.bodyToFlux(Item.class))
                 .log("Items in Client Project exchange :");
     }
+
+    @GetMapping("/client/retrieve/singleItem/{id}")
+    public Mono<Item> getOneItemUsingRetrieve(@PathVariable String id) {
+
+        return webClient.get().uri("/v1/items/{id}", id)
+                .retrieve()
+                .bodyToMono(Item.class)
+                .log("Items in Client Project retrieve single item :");
+    }
+
+    @GetMapping("/client/exchange/singleItem/{id}")
+    public Mono<Item> getOneItemUsingExchange(@PathVariable String id) {
+
+        return webClient.get().uri("/v1/items/{id}", id)
+                .exchange()
+                .flatMap(clientResponse -> clientResponse.bodyToMono(Item.class))
+                .log("Items in Client Project exchange single item :");
+    }
+
 
 }
